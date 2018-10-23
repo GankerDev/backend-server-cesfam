dbb6f50a7568093f5d93de47cdfd27945bb9245b
Ganker <joshua.cid@virginiogomez.cl>
2018-10-23 06:18:20 -0300

    Autenticacion google

--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = 'I@wait@to@see@the@light';
+
+// Google
+module.exports.CLIENT_ID = '52291293785-3mlr11c0j6u92votctu4q0s82sgbd3n5.apps.googleusercontent.com';
--- models/usuario.js
+++ models/usuario.js
@@ -15,8 +15,8 @@ var usuarioSchema = new Schema({
     nombre: { type: String, required: [true, 'El nombre es necesario'] },
     password: { type: String, required: [true, 'La contraseña es necesaria'] },
     img: { type: String, required: false },
-    role: { type: String, required: true, default: 'USER_ROLE', enum: rolesValidos }
-
+    role: { type: String, required: true, default: 'USER_ROLE', enum: rolesValidos },
+    google: { type: Boolean, default: false }
 });
 
 usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe ser único' });
